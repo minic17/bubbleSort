612a7397c062ee2dbafc60301deebdd44914fa36
minic17 <40836361+minic17@users.noreply.github.com>
2020-04-24 18:57:57 -0700

    PEP8 changes
    Brought into compliance with PEP8 widely accepted styling guide requirements

--- bubbleSort.py
+++ bubbleSort.py
@@ -4,12 +4,11 @@ import random
 def bubbleSort(input_list):
 
     listLength = len(input_list)
-    
     while(True):
-        superTrue=[]
-        i=0
+        superTrue = []
+        i = 0
         while(i < listLength-1):
-            if(input_list[i]<input_list[i+1]):
+            if(input_list[i] < input_list[i+1]):
                 tempVar = input_list[i]
                 input_list[i] = input_list[i+1]
                 input_list[i+1] = tempVar
@@ -18,13 +17,12 @@ def bubbleSort(input_list):
         if(len(superTrue) == 0):
             print(input_list)
             break
-        
 
-#testing code written by minic17 2020 ALL RIGHTS RESERVED
+# testing code written by minic17 2020 ALL RIGHTS RESERVED
 n = 100
 l = []
 for i in range(n):
-    l.append(random.randint(1,1000))
+    l.append(random.randint(1, 1000))
 print(l)
 bubbleSort(l)
 print(l)
